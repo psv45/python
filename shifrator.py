4b9185593319ab21b56fefbd48392a9d0cbe1e54
psv45 <sergeymorello@gmail.com>
2021-03-15 23:05:06 +0500

    commit

--- shifrator.py
+++ shifrator.py
@@ -1,15 +1,22 @@
 action = int(input('1 - зашифровать, 2 - расшифровать\n'))
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 shifr=''
 if action == 1:
     text = input('Введите текст для шифрования\n')
-    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
     secret_key = 3
     for i in text:
         if alphabet.find(i)+secret_key<=len(alphabet):
             shifr = shifr + alphabet[alphabet.find(i)+secret_key]
         if alphabet.find(i)+secret_key>len(alphabet):
             shifr = shifr + alphabet[(alphabet.find(i)+secret_key)%len(alphabet)]
-    print(shifr)
-
-
-
+    print('Зашифрованное слово:', shifr)
+    print('Ключ:', secret_key)
+elif action == 2:
+    text = input('Введите текст для расшифровки\n')
+    secret_key = int(input('Введите ключ\n'))
+    for i in text:
+        if alphabet.find(i)-secret_key>=1:
+            shifr = shifr + alphabet[alphabet.find(i)-secret_key]
+        if alphabet.find(i)-secret_key<1:
+            shifr = shifr + alphabet[(alphabet.find(i)-secret_key+len(alphabet))%len(alphabet)]
+    print('Расшифрованное слово:', shifr)
